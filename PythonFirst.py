f7222b8b233250092ec4632d8e6e53da2c85035f
Kiran Kumar Tirunandam Muralikrishna <123753360+tmkirankumar786@users.noreply.github.com>
2024-07-26 20:06:55 +0530

    Add files via upload

---
+++ PythonFirst.py
@@ -0,0 +1,19 @@
+# Python for ML/AI
+
+print("This is AIML Prgrom Test File")
+
+
+#x = int(input("Enter the x number"))
+#y = int(input("Enter the y number"))
+#z = int(input("Enter the z number"))
+
+# if (x>y and x>z):
+#    print("x is highest")
+# elif(y>z):
+#    print("y is highest")
+# else:
+#    print("z is highest")
+
+
+
+
